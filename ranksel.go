492213df3430194d38829bb98834b76dc6f64deb
Jeff Wendling <leterip@gmail.com>
2019-07-27 06:44:56 -0600

    some wip

--- ranksel.go
+++ ranksel.go
@@ -22,82 +22,112 @@ import (
 //
 
 type rsqfData struct {
-	buf     []byte // backing array
-	quo     uint   // bits per quotient
-	quoMask uint64 // mask for quotient
-	rem     uint   // bits per remainder
-	remMask uint64 // mask for remainder
-	block   uint64 // size of a block. 17 + 8*rem
+	base      unsafe.Pointer // backing array
+	block     uint64         // size of a block. 17 + 8*rem
+	blockMask uint64         // mask of number of blocks
+	quoBits   uint           // bits per quotient
+	quoMask   uint64         // mask for quotient
+	remBits   uint           // bits per remainder
+	remMask   uint64         // mask for remainder
 }
 
-func newRSQFData(buf []byte, quo, rem uint) *rsqfData {
+// newRSQFData returns an abstraction around rsqf data. base must be a pointer to
+// a buffer of bytes 2^quo * (rem + 2.125) + 7 bytes, and quo must be at least 6.
+func newRSQFData(base *byte, quo, rem uint) *rsqfData {
 	return &rsqfData{
-		buf:     buf,
-		quo:     quo,
-		quoMask: 1<<quo - 1,
-		rem:     rem,
-		remMask: 1<<rem - 1,
-		block:   17 + 8*uint64(rem),
+		base:      unsafe.Pointer(base),
+		quoBits:   quo,
+		quoMask:   1<<quo - 1,
+		remBits:   rem,
+		remMask:   1<<rem - 1,
+		block:     18 + 8*uint64(rem),
+		blockMask: 1<<(quo-6) - 1,
 	}
 }
 
-// Offset returns a pointer to the ith offset, which is the offset for the
-// i*64th quotient.
-func (r *rsqfData) Offset(i uint64) *uint8 {
-	return &r.buf[r.block*i]
+// rsqfBlock is a struct matching the layout of a block of data for easy code manipulation.
+// it is important to use u64 so that padding is not inserted.
+type rsqfBlock struct {
+	offset   uint8
+	occupied u64
+	runends  u64
+	// 512 is definitely too large by 8 bytes, so we're safe to
+	// do a raw read of 8 bytes at any point up to 512.
+	rems [512]byte
 }
 
-// Occupied returns a pointer to the ith occupied vector, which contains the occupied
-// bits for the quotients in [64 * i, 64 * i + 64).
-func (r *rsqfData) Occupied(i uint64) *u64 {
-	off := r.block*i + 1
-	return (*u64)(unsafe.Pointer(&r.buf[:off+8][off]))
+// enforce at compile time the offsets of the individual fields.
+type _ struct {
+	// offset is at offset 0
+	_ [unsafe.Offsetof(rsqfBlock{}.offset) - 0]struct{}
+	_ [0 - unsafe.Offsetof(rsqfBlock{}.offset)]struct{}
+
+	// occupied is at offset 1
+	_ [unsafe.Offsetof(rsqfBlock{}.occupied) - 1]struct{}
+	_ [1 - unsafe.Offsetof(rsqfBlock{}.occupied)]struct{}
+
+	// runends is at offset 9
+	_ [unsafe.Offsetof(rsqfBlock{}.runends) - 9]struct{}
+	_ [9 - unsafe.Offsetof(rsqfBlock{}.runends)]struct{}
+
+	// rems is at offset 17
+	_ [unsafe.Offsetof(rsqfBlock{}.rems) - 17]struct{}
+	_ [17 - unsafe.Offsetof(rsqfBlock{}.rems)]struct{}
 }
 
-// Runends returns a pointer to the ith runends vector, which contains the runends
-// information for the bits in [64 * i, 64 * i + 64).
-func (r *rsqfData) Runends(i uint64) *u64 {
-	off := r.block*i + 9
-	return (*u64)(unsafe.Pointer(&r.buf[:off+8][off]))
+// getU64 reads the u64 at the provided byte offset in remainders.
+func (bl *rsqfBlock) getU64(b uint) *u64 {
+	return (*u64)(unsafe.Pointer(&bl.rems[b%512]))
 }
 
-// Remainders returns a bit reader for the ith remainders vector, which contains the
-// remainders for the slots in [64 * i, 64 * i + 64).
-func (r *rsqfData) Remainders(i uint64) bitReader {
-	off := r.block*i + 17
-	return newBitReader(r.buf[off:off+uint64(r.rem)*8], r.rem)
+// getBlock returns the ith block, wrapping around the number of blocks.
+func (r *rsqfData) getBlock(i uint64) *rsqfBlock {
+	offset := uintptr((i & r.blockMask) * r.block)
+	return (*rsqfBlock)(unsafe.Pointer(uintptr(r.base) + offset))
 }
 
-// Rank returns the number of set bits of the occupied bit vector starting at the
+// getRemainder reads the remainder starting at the ith bit.
+func (r *rsqfData) getRemainder(bl *rsqfBlock, i uint) uint64 {
+	return bl.getU64(i/8).toUint64() >> (i % 8)
+}
+
+// putRemainder stores rem in the ith bit using the mask to select bits.
+func (r *rsqfData) putRemainder(bl *rsqfBlock, i uint, rem uint64) {
+	o := i % 8                  // compute the unaligned amount
+	v := bl.getU64(i / 8)       // get the pointer aligned to a byte
+	u := v.toUint64()           // get the value out
+	u &^= r.remMask << o        // clear the bits we're going to be setting
+	u |= (rem & r.remMask) << o // set the bits from the value
+	*v = toU64(u)               // write it back
+}
+
+// occupiedRank returns the number of set bits of the occupied bit vector starting at the
 // sth bit and stopping at the (s+b)th bit. b is at most 64.
-func (r *rsqfData) OccupiedRank(s, b uint64) uint {
+func (r *rsqfData) occupiedRank(s, b uint64) uint {
 	idx, off := s/64, s%64
 
 	// we remove off lower order bits and keep at most b higher order bits.
-	occ := r.Occupied(idx).toUint64()
+	occ := r.getBlock(idx).occupied.toUint64()
 	occ >>= off
-	occ <<= (64 - b) % 64
+	occ <<= 64 - (b % 64)
 	rank := uint(bits.OnesCount64(occ))
 
 	// if we overflow a single uint64, then grab the next one.
 	if shift := 128 - off - b; shift < 64 {
-		occ = r.Occupied(idx + 1).toUint64()
+		occ = r.getBlock(idx + 1).occupied.toUint64()
 		occ <<= shift
 		rank += uint(bits.OnesCount64(occ))
 	}
 
-	fmt.Printf("--- rank from bit %d for %d bits: %d\n", s, b, rank)
 	return rank
 }
 
-// SelectRunends returns the number of bits past s until the bth bit is set.
-func (r *rsqfData) RunendsSelect(s, b uint64) uint {
+// runendsSelect returns the number of bits past s until the bth bit is set.
+func (r *rsqfData) runendsSelect(s, b uint64) uint {
 	idx, off, acc := s/64, uint(s%64), uint(0)
-	fmt.Printf("--- selecting after bit %d for %d bits: ", s, b)
 
 check:
-	run := r.Runends(idx).toUint64()
-	run >>= off
+	run := r.getBlock(idx).runends.toUint64() >> off
 
 	// use popcount to traverse a word at a time.
 	if count := uint64(bits.OnesCount64(run)); count <= b {
@@ -129,71 +159,60 @@ check:
 		run &= run - 1
 	}
 
-	fmt.Println(acc + uint(bits.TrailingZeros64(run)))
 	return acc + uint(bits.TrailingZeros64(run))
-}
-
-// QuotientSlot returns the furthest slot that ends a run for some
-// quotient in [q / 64 * 64, q].
-func (r *rsqfData) QuotientSlot(q uint64) uint64 {
-	rep := q / 64 * 64
-
-	off := uint64(*r.Offset(q / 64))
-	if rep == q {
-		return q + off
-	}
-
-	d := uint64(r.OccupiedRank(rep+1, q-rep-1))
-	if d == 0 {
-		return rep + off
-	}
-
-	t := uint64(r.RunendsSelect(rep+off+1, d))
-	return rep + off + t + 1
 }
 
 // Lookup reports true for any hash that has been inserted and possibly for some
 // hashes that have not been inserted. If it ever reports false, then the hash
 // has definitely not been inserted.
 func (r *rsqfData) Lookup(hash uint64) bool {
+	// break out the remainder and quotient.
 	rem := hash & r.remMask
-	hash >>= r.rem
-	quo := hash & r.quoMask
+	quo := (hash >> (r.remBits % 64)) & r.quoMask
 
-	if r.Occupied(quo/64).toUint64()&(1<<quo%64-1) == 0 {
+	fmt.Printf("L find hash:%d quo:%d rem:%d\n", hash, quo, rem)
+
+	// grab the block for the provided hash.
+	idx, off := quo/64, uint(quo%64)
+	bl := r.getBlock(idx)
+
+	// if the quotient isn't occupied, we're done.
+	if bl.occupied.toUint64()>>off&1 == 0 {
+		fmt.Println("L find unoccupied")
 		return false
 	}
 
-	slot := r.QuotientSlot(quo)
-	block := slot / 64
-	rems := r.Remainders(block)
-	runs := r.Runends(block).toUint64()
-	idx := uint(slot % 64)
-	sel := uint64(1) << idx
+	// compute where the end of the run containing the quotient would be.
+	slot := r.rankSelect(quo)
 
-next:
-	slotRem := rems.Get(idx)
+	// grab the block for the computed index.
+	idx, off = slot/64, uint(slot%64)
+	bl = r.getBlock(idx)
 
-	if slotRem == rem {
-		return true
-	} else if slotRem < rem || runs&sel != 0 {
-		return false
-	}
+	sel := uint64(1) << off
+	runs := bl.runends.toUint64()
 
-	if idx > 0 {
-		idx--
-		sel >>= 1
-		goto next
-	} else if block > 0 {
-		block--
-		rems = r.Remainders(block)
-		runs = r.Runends(block).toUint64()
-		idx = 63
+	for {
+		// check for remainder matching, remainder never matching, or run ending.
+		if slotRem := r.getRemainder(bl, r.remBits*off) & r.remMask; slotRem == rem {
+			return true
+		} else if slotRem < rem || runs&sel != 0 {
+			return false
+		}
+
+		// walk back an offset if possible.
+		if off > 0 {
+			off--
+			sel >>= 1
+			continue
+		}
+
+		// walk back a block.
+		idx, off = idx-1, 63
+		bl = r.getBlock(idx)
 		sel = 1 << 63
-		goto next
+		runs = bl.runends.toUint64()
 	}
-
-	return false
 }
 
 // Insert adds the hash to the filter so that Lookup will definitely report
@@ -201,35 +220,169 @@ next:
 // further operations should be performed on it. This should never happen if
 // the hashes are randomly distributed.
 func (r *rsqfData) Insert(hash uint64) bool {
+	defer func() {
+		// runends should always have the same number of total bits as occupied
+		occ, rem := 0, 0
+		for j := uint64(0); j < 1<<r.quoBits; j++ {
+			bl := r.getBlock(j)
+			occ += bits.OnesCount64(bl.occupied.toUint64())
+			rem += bits.OnesCount64(bl.runends.toUint64())
+		}
+		if occ != rem {
+			panic("mismatch")
+		}
+	}()
+
+	// break out the remainder and quotient.
 	rem := hash & r.remMask
-	hash >>= r.rem
-	quo := hash & r.quoMask
-	qblock, qidx := quo/64, uint(quo%64)
+	quo := (hash >> (r.remBits % 64)) & r.quoMask
 
-	slot := r.QuotientSlot(quo)
-	fmt.Printf("quo:%d rem:%d slot:%d\n", quo, rem, slot)
+	fmt.Printf("I set  hash:%d quo:%d rem:%d\n", hash, quo, rem)
 
-	occs := r.Occupied(qblock)
-	if quo > slot || (quo == slot && quo == qblock*64 && occs.toUint64()&(1<<qidx) == 0) {
-		rems := r.Remainders(qblock)
-		rems.Put(qidx, rem)
-		occs := r.Occupied(qblock)
-		*occs = toU64(occs.toUint64() | 1<<qidx)
-		ends := r.Runends(qblock)
-		*ends = toU64(ends.toUint64() | 1<<qidx)
+	// grab the block for the quotient to begin the search.
+	idx, off := quo/64, uint(quo%64)
+	bl := r.getBlock(idx)
+
+	// compute where the end of the run for the quotient is.
+	slot := r.rankSelect(quo)
+	fmt.Printf("I init quo:%d rem:%d idx:%d off:%d slot:%d\n", quo, rem, idx, off, slot)
+
+	// if the slot is before our quotient, or equal to it and it's the first
+	// quotient inside of the block, then the quotient is missing and the
+	// slot is empty.
+	if slot < quo {
+		r.putRemainder(bl, r.remBits*off, rem)
+		bl.occupied = toU64(bl.occupied.toUint64() | 1<<off)
+		bl.runends = toU64(bl.runends.toUint64() | 1<<off)
 		return true
 	}
 
-	return false
+	// save the location of the quotient for later.
+	qbl, qoff := bl, off
+
+	// the spot we're storing into is the next slot.
+	slot++
+
+	// grab the block containing the first unused value past the slot.
+	last, numRuns := r.findFirstUnused(slot)
+	idx, off = last/64, uint(last%64)
+	roff := r.remBits * off
+	bl = r.getBlock(idx)
+	runs := bl.runends.toUint64()
+
+	fmt.Printf("I find slot:%d last:%d quo:%d runs:%d\n", slot, last, quo, numRuns)
+	quo += numRuns // keep track of what quotient the slot is in
+
+	// copy things backwards until we get to slot.
+	for last > slot {
+		last--
+
+		// figure out what our next state is.
+		nbl, nidx, noff, nroff, nruns := bl, idx, off, roff, runs
+		if off > 0 {
+			noff--
+			nroff -= r.remBits
+		} else {
+			nidx--
+			noff = 63
+			nroff = r.remBits * 63
+			nbl = r.getBlock(nidx)
+			nruns = nbl.runends.toUint64()
+		}
+
+		// keep track of if a run is ending.
+		nends := nruns >> noff & 1
+
+		// unconditionally set the remainder to the next one.
+		r.putRemainder(bl, roff, r.getRemainder(nbl, nroff))
+
+		// check if the next runends bit is different, and if so, flip it.
+		fmt.Printf("I flip idx:%d off:%d quo:%d ends:%t\n", idx, off, quo, runs>>off&1 != nends)
+		if runs>>off&1 != nends {
+			runs ^= 1 << off
+		}
+
+		// update runends when we switch blocks
+		if bl != nbl {
+			bl.runends = toU64(runs)
+		}
+
+		// if we're on a representitive quotient, increment the offset
+		if quo&63 == 0 {
+			bl.offset++
+		}
+		quo -= nends
+
+		// update to the next state.
+		bl, idx, off, roff = nbl, nidx, noff, nroff
+	}
+
+	// always assign in case the loop didn't end on another block
+	bl.runends = toU64(runs)
+
+	// always bump the offset if we're on a represntitive quotient.
+
+	fmt.Printf("I set  slot:%d last:%d quo:%d idx:%d off:%d roff:%d\n", slot, last, quo, idx, off, roff)
+
+	// last == slot now, so we know that bl points into the right block, and
+	// idx, off and roff are the appropriate values.
+	r.putRemainder(bl, roff, rem)
+	bl.runends = toU64(bl.runends.toUint64() | 1<<off)
+	if off == 0 {
+		bl.offset++
+	}
+
+	// if the quotient is unoccupied, flag it as occupied and we're done.
+	if qbl.occupied.toUint64()>>qoff&1 == 0 {
+		qbl.occupied = toU64(qbl.occupied.toUint64() | 1<<qoff)
+		return true
+	}
+
+	// otherwise, clear the previous runends bit and bump the offset if
+	// our quotient is a representitive quotient.
+	if off > 0 {
+		fmt.Printf("I del  idx:%d off:%d\n", idx, off-1)
+		bl.runends = toU64(bl.runends.toUint64() &^ (1 << (off - 1)))
+	} else {
+		fmt.Printf("I del  idx:%d off:%d\n", idx-1, 63)
+		nbl := r.getBlock(idx - 1)
+		nbl.runends = toU64(nbl.runends.toUint64() &^ (1 << 63))
+	}
+	if quo&63 == 0 {
+		bl.offset++
+	}
+
+	// if we're at an offset of 255, more adds are unsafe.
+	return bl.offset < 255
 }
 
-// findUnused finds the first unused slot after the quotient.
-func (r *rsqfData) findUnused(quo uint64) uint64 {
-next:
-	end := r.QuotientSlot(quo)
-	if quo > end {
-		return quo
+func (r *rsqfData) rankSelect(slot uint64) (uint64, bool) {
+	rep := slot &^ 63
+	off := uint64(r.getBlock(slot / 64).offset)
+	b := slot - rep + 1
+
+	d := uint64(r.occupiedRank(rep+off, b))
+	if d == 0 {
+		fmt.Printf("R dead kind:rank quo:%d d:%d b:%d\n", slot, d, b)
+		return 0, false
 	}
-	quo = end + 1
-	goto next
+
+	t := uint64(r.runendsSelect(rep+off, d))
+	fmt.Printf("R rank kind:sel quo:%d slot:%d d:%d b:%d t:%d off:%d\n", slot, rep+off+t, d, b, t, off)
+	return rep + off + t, true
+}
+
+// findFirstUnused finds the first unused slot after the quotient.
+func (r *rsqfData) findFirstUnused(quo uint64) (uint64, uint64) {
+	runs := uint64(0)
+	for j := uint64(0); j < r.quoMask; j++ {
+		slot, ok := r.rankSelect(quo)
+		if quo > slot || !ok {
+			return quo, runs
+		}
+		quo = slot + 1
+		runs++
+	}
+
+	panic("too many iterations")
 }
--- ranksel_test.go
+++ ranksel_test.go
@@ -4,94 +4,132 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math"
+	"math/bits"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/zeebo/assert"
+	"github.com/zeebo/pcg"
 )
+
+func hexBlock(b *rsqfBlock) {
+	fmt.Printf("---\noff: %x\nocc: %x\nrun: %x\nrem: %x\n",
+		b.offset, b.occupied.toUint64(), b.runends.toUint64(),
+		binary.LittleEndian.Uint64(b.rems[:8]))
+}
+
+func binBlock(b *rsqfBlock) {
+	fmt.Printf("---\noff: %08b\nocc: %064b\nrun: %064b\nrem: %064b\n",
+		b.offset, b.occupied.toUint64(), b.runends.toUint64(),
+		binary.LittleEndian.Uint64(b.rems[:8]))
+}
+
+func binBlock2(b, c *rsqfBlock) {
+	fmt.Printf("occ: %064b %064b\nrun: %064b %064b\nrem: %064b %064b\noff: %-3d %-3d\n",
+		bits.Reverse64(b.occupied.toUint64()), bits.Reverse64(c.occupied.toUint64()),
+		bits.Reverse64(b.runends.toUint64()), bits.Reverse64(c.runends.toUint64()),
+		bits.Reverse64(binary.LittleEndian.Uint64(b.rems[:8])),
+		bits.Reverse64(binary.LittleEndian.Uint64(c.rems[:8])),
+		b.offset, c.offset)
+}
 
 func TestRSQFData(t *testing.T) {
 	t.Run("Rank", func(t *testing.T) {
 		buf := make([]byte, 1024) // way too big
-		data := newRSQFData(buf, 1, 1)
+		data := newRSQFData(&buf[0], 7, 1)
 
-		*data.Occupied(0) = toU64(math.MaxUint64)
-		*data.Occupied(1) = toU64(math.MaxUint64)
+		data.getBlock(0).occupied = toU64(math.MaxUint64)
+		data.getBlock(1).occupied = toU64(math.MaxUint64)
 
-		assert.Equal(t, data.OccupiedRank(0, 1), uint(1))
-		assert.Equal(t, data.OccupiedRank(60, 20), uint(20))
+		assert.Equal(t, data.occupiedRank(0, 1), uint(1))
+		assert.Equal(t, data.occupiedRank(60, 20), uint(20))
+
+		data.getBlock(2).occupied = toU64(0x5555555555555555)
+		assert.Equal(t, data.occupiedRank(64*2+1, 15), uint(7))
 	})
 
 	t.Run("Select", func(t *testing.T) {
 		buf := make([]byte, 1024) // way too big
-		data := newRSQFData(buf, 1, 1)
+		data := newRSQFData(&buf[0], 1, 1)
 
-		*data.Runends(0) = toU64(math.MaxUint64)
-		*data.Runends(1) = toU64(math.MaxUint64)
-		*data.Runends(2) = toU64(math.MaxUint64)
-		*data.Runends(3) = toU64(math.MaxUint64)
-		*data.Runends(4) = toU64(math.MaxUint64)
-		*data.Runends(5) = toU64(2)
+		data.getBlock(0).runends = toU64(math.MaxUint64)
+		data.getBlock(1).runends = toU64(math.MaxUint64)
+		data.getBlock(2).runends = toU64(math.MaxUint64)
+		data.getBlock(3).runends = toU64(math.MaxUint64)
+		data.getBlock(4).runends = toU64(math.MaxUint64)
+		data.getBlock(5).runends = toU64(2)
 
-		assert.Equal(t, data.RunendsSelect(0, 0), uint(0))
+		assert.Equal(t, data.runendsSelect(0, 0), uint(0))
 		for i := uint64(0); i < 320; i++ {
-			assert.Equal(t, data.RunendsSelect(i, 320-i-1), 320-i-1)
+			assert.Equal(t, data.runendsSelect(i, 320-i), 320-i-1)
 		}
 	})
 
 	t.Run("Insert", func(t *testing.T) {
-		buf := make([]byte, 2*(17+8))
-		data := newRSQFData(buf, 7, 1)
+		buf := make([]byte, 2*(17+8)+7)
+		data := newRSQFData(&buf[0], 7, 1)
 
-		p := func(block uint64) {
-			fmt.Printf("off:%d occ:%064b end:%064b rem:%064b\n",
-				*data.Offset(block),
-				data.Occupied(block).toUint64(),
-				data.Runends(block).toUint64(),
-				binary.LittleEndian.Uint64(data.Remainders(block).buf))
+		// insert rem 0 into every even quotient
+		for i := uint64(0); i < 128; i += 2 {
+			assert.That(t, data.Insert(i<<1))
 		}
 
-		data.Insert(0 << 1)
-		p(0)
-		p(1)
+		// insert into random even slots to fill it up
+		for i := 0; i < 64; i++ {
+			quo := pcg.Uint64() % 64 * 2
+			assert.That(t, data.Insert(quo<<1))
+		}
 
-		data.Insert(1 << 1)
-		p(0)
-		p(1)
+		// look up every value. odd with a fingerprint of 1 and
+		// even with a fingerprint of 0 so that we have no false
+		// positives.
+		for i := uint64(0); i < 128; i++ {
+			assert.That(t, data.Lookup(i) == (i&3 == 0))
+		}
+	})
 
-		data.Insert(64 << 1)
-		p(0)
-		p(1)
+	t.Run("Dense", func(t *testing.T) {
+		rng := pcg.New(uint64(time.Now().UnixNano()))
+		buf := make([]byte, 2*(17+8)+7)
+		data := newRSQFData(&buf[0], 7, 1)
 
-		data.Insert(65 << 1)
-		p(0)
-		p(1)
+		// fill almost all of it
+		for i := 0; i < 120; i++ {
+			quo := rng.Uint64() % 128
+			fmt.Printf("%sv\n", strings.Repeat(" ", int(5+quo+quo/64)))
+			binBlock2(data.getBlock(0), data.getBlock(1))
+			assert.That(t, data.Insert(quo<<1|1))
+			assert.That(t, data.Lookup(quo<<1|1))
+			fmt.Println()
+		}
 	})
 }
 
 func BenchmarkRSQFData(b *testing.B) {
 	buf := make([]byte, 1024) // way too big
-	data := newRSQFData(buf, 1, 1)
+	data := newRSQFData(&buf[0], 1, 1)
 
-	*data.Occupied(0) = toU64(math.MaxUint64)
-	*data.Occupied(1) = toU64(math.MaxUint64)
+	data.getBlock(0).occupied = toU64(math.MaxUint64)
+	data.getBlock(1).occupied = toU64(math.MaxUint64)
 
-	*data.Runends(0) = toU64(math.MaxUint64)
-	*data.Runends(1) = toU64(math.MaxUint64)
-	*data.Runends(2) = toU64(math.MaxUint64)
-	*data.Runends(3) = toU64(math.MaxUint64)
-	*data.Runends(4) = toU64(math.MaxUint64)
-	*data.Runends(5) = toU64(2)
+	data.getBlock(0).runends = toU64(math.MaxUint64)
+	data.getBlock(1).runends = toU64(math.MaxUint64)
+	data.getBlock(2).runends = toU64(math.MaxUint64)
+	data.getBlock(3).runends = toU64(math.MaxUint64)
+	data.getBlock(4).runends = toU64(math.MaxUint64)
+	data.getBlock(5).runends = toU64(2)
 
 	b.Run("Rank", func(b *testing.B) {
 		b.Run("Easy", func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				data.OccupiedRank(0, 1)
+				data.occupiedRank(0, 1)
 			}
 		})
 
 		b.Run("Hard", func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				data.OccupiedRank(60, 20)
+				data.occupiedRank(60, 20)
 			}
 		})
 	})
@@ -99,13 +137,50 @@ func BenchmarkRSQFData(b *testing.B) {
 	b.Run("Select", func(b *testing.B) {
 		b.Run("Easy", func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				data.RunendsSelect(0, 0)
+				data.runendsSelect(0, 0)
 			}
 		})
 
 		b.Run("Hard", func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				data.RunendsSelect(256, 63)
+				data.runendsSelect(256, 63)
+			}
+		})
+	})
+
+	b.Run("Insert", func(b *testing.B) {
+		b.Run("50", func(b *testing.B) {
+			buf := make([]byte, 4096)
+			data := newRSQFData(&buf[0], 10, 1)
+
+			for i := 0; i < b.N; i++ {
+				// fill it half full
+				for j := 0; j < 512; j++ {
+					data.Insert(pcg.Uint64())
+				}
+
+				b.StopTimer()
+				for i := range buf {
+					buf[i] = 0
+				}
+				b.StartTimer()
+			}
+		})
+
+		b.Run("95", func(b *testing.B) {
+			buf := make([]byte, 4096)
+			data := newRSQFData(&buf[0], 10, 1)
+
+			for i := 0; i < b.N; i++ {
+				// fill it 95%
+				for j := 0; j < 973; j++ {
+					data.Insert(pcg.Uint64())
+				}
+
+				// reset the state
+				for i := range buf {
+					buf[i] = 0
+				}
 			}
 		})
 	})
